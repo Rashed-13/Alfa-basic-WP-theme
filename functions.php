769baa9b72ca7972614c58ffd731c48b085bdd7d
Rashed <jmrashed.13@gmail.com>
2022-11-25 21:55:59 +0600

    Sitebar added/registered

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 function alfa_bootstrappig(){
     load_theme_textdomain("alfa");
     add_theme_support("title-tag");
@@ -9,12 +8,28 @@ function alfa_bootstrappig(){
 
 add_action("after_setup_theme", "alfa_bootstrappig");
 
-
 function alfa_assets(){
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
     wp_enqueue_style("alfa", get_stylesheet_uri(), null,the_date(), null);
 }
 add_action("wp_enqueue_scripts", "alfa_assets");
+
+
+
+function alfa_sidebar(){
+    register_sidebar(array(
+        "name"          => __('Single post sitebar', 'alfa'),
+        "id"            => "sitebar1",
+        "description"   => __("Right sitebar 1", "alfa"),
+        "before_widget" => "<div>",
+        "after_widget"  => "</div>",
+        "before_title"  => "<h2>",
+        "after_title"   => "</h2>",
+        "before_sidebar"=> "<section>",
+        "after_sidebar" => "</section>"
+    ));
+}
+add_action("widgets_init", "alfa_sidebar")
 
 
 
--- single.php
+++ single.php
@@ -1,61 +1,74 @@
 <?php get_header();?>
 <body <?php body_class();?>>
 <?php get_template_part("hero");?>
-<div class="posts">
-    <div class="post" <?php post_class();?>>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <h2 class="post-title text-center">
-                        <?php the_title();?>
-                    </h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-10 offset-md-1 text-center">
-                    <p>
-                        <strong><?php the_post(); the_author(); rewind_posts()?></strong><br/>
-                        <?php echo get_the_date();?>
-                    </p>
-                    <?php 
-                        echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>");
-                    ?>
-                </div>
-                <div class="col-md-10 offset-md-1">
-                    <?php if(has_post_thumbnail()){
-                        the_post_thumbnail("large", "class='img-fluid'");
-                    } ?>
-                    <p>
-                        <?php 
-                            the_content();
-                        ?>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="container">
     <div class="row">
-        <?php if(comments_open()):;?>
-            <div class="col-md-10 offset-1">
-                <?php 
-                comments_template()?>
+        <div class="col-md-8">
+            <div class="posts">
+                <div class="post" <?php post_class();?>>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 ">
+                                <h2 class="post-title text-center">
+                                    <?php the_title();?>
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <p>
+                                    <strong><?php the_post(); the_author(); rewind_posts()?></strong><br/>
+                                    <?php echo get_the_date();?>
+                                </p>
+                                <?php 
+                                    echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>");
+                                ?>
+                            </div>
+                            <div class="col-md-12">
+                                <?php if(has_post_thumbnail()){
+                                    the_post_thumbnail("large", "class='img-fluid'");
+                                } ?>
+                                <p>
+                                    <?php 
+                                        the_content();
+                                    ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        <?php endif;?>
-    </div>
-    <div class="row">
-        <div class="col-md-4 offset-md-1">
-        <?php 
-            next_post_link('%link', 'Previous post'); 
-        ?>
+            <div class="container">
+                <div class="row">
+                    <?php if(comments_open()):;?>
+                        <div class="col-md-12">
+                            <?php 
+                            comments_template()?>
+                        </div>
+                    <?php endif;?>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php 
+                            next_post_link('%link', 'Previous post'); 
+                        ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?php 
+                            previous_post_link('%link', 'Next post'); 
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4 offset-md-1">
+        <div class="col-md-4">
             <?php 
-                previous_post_link('%link', 'Next post'); 
+                if(is_active_sidebar("sitebar1")){
+                    dynamic_sidebar("sitebar1");
+                }
             ?>
         </div>
     </div>
 </div>
+
 <?php get_footer();?>
