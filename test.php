19e777bff10052facd75c85d613bd59bbbc53276
Rashed <jmrashed.13@gmail.com>
2022-12-08 23:57:58 +0600

    Child theme added

--- test.php
+++ test.php
@@ -1,11 +1,6 @@
 <?php 
 
-function full_name( string $name, $callBack, $arr){
-    echo "Your full name is {$name}\n";
-    $callBack("your girl-friend is", $arr);
-}
+ $pattern = '/('.join('|',explode(' ', "Rashed khan bahadur")).')/i';
 
-function callback($text, $glist){
-    echo "Now {$text} {$glist[1]}";
-}
-full_name("Rashed", "callback", array("Momo", "Khadiza"));
+
+echo $pattern;
